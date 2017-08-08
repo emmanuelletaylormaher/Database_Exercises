7b96510626be5193222f23b19f04da3074bd4b37
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-08 11:52:52 -0500

    added delete_exercises to path

---
+++ delete_exercises.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+-- Write SELECT statements for:
+-- Albums released after 1991
+DELETE
+FROM albums
+WHERE release_date > 1991;
+
+-- Albums with the genre 'disco'
+DELETE
+FROM albums
+WHERE genre LIKE "%disco%";
+
+-- Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+DELETE
+FROM albums
+WHERE artist LIKE "%Whitney%";
