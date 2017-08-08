e445ad950417b03f1df9ae4ea8eb0e164d493cc3
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-08 15:35:55 -0500

    Refactored query in which items are GROUPED BY uniqeuness

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -5,4 +5,11 @@ FROM titles;
 -- Update the previous query to sort the results alphabetically.
 SELECT DISTINCT title
 FROM titles
-ORDER BY title ASC;
+GROUP BY title ASC;
+
+-- Find your query for employees whose last names start and end with 'E'.
+-- Update the query find just the unique last names that start and end with 'E' using GROUP BY.
+SELECT DISTINCT *
+FROM employees
+WHERE last_name LIKE "E%e"
+GROUP BY last_name;
