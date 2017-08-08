266b93268188ece52c709868eb2f5ae414a521ad
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-08 15:31:43 -0500

    Updated query to sort titles alphabetically

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,3 +1,8 @@
 -- In your script, use DISTINCT to find the unique titles in the titles table.
 SELECT DISTINCT title
 FROM titles;
+
+-- Update the previous query to sort the results alphabetically.
+SELECT DISTINCT title
+FROM titles
+ORDER BY title ASC;
